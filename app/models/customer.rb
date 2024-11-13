8bd53a8791711f7af41fac00cbba9bb10e8c452f
Kyle Delaney <khdelaney99@gmail.com>
2024-11-12 22:12:52 -0500

    add validations for customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,12 @@
 class Customer < ApplicationRecord
     has_many :subscriptions
     has_many :teas, through: :subscriptions
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: {message: "Email is in use, try another."}
+    validates :street_address, presence: true
+    validates :city, presence: true
+    validates :state, presence: true
+    validates :zip, presence: true, numericality: { message: "Zip must be a number." }
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it {should have_many(:teas).through(:subscriptions)}
+    it {should have_many(:subscriptions)}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of(:first_name)}
+    it {should validate_presence_of(:last_name)}
+    it {should validate_presence_of(:email)}
+    it {should validate_presence_of(:street_address)}
+    it {should validate_presence_of(:city)}
+    it {should validate_presence_of(:state)}
+    it {should validate_presence_of(:zip)}
+
+
+    it 'validates the numericality of zip' do
+      customer = Customer.new(
+        first_name: "Monkey",
+        last_name: "Luffy",
+        email: "mluffy@aol.com",
+        street_address: "1738",
+        city: "Fosha Village",
+        state: "East Blue",
+        zip: "swag"
+      )
+
+      expect(customer).not_to be_valid
+      expect(customer.errors[:zip]).to include("Zip must be a number.")
+    end
+
+    it 'validates the uniqueness of email' do
+      customer1 = Customer.create!(
+        first_name: "Monkey",
+        last_name: "Luffy",
+        email: "mluffy@aol.com",
+        street_address: "1738",
+        city: "Fosha Village",
+        state: "East Blue",
+        zip: 17171
+      )
+      customer2 = Customer.new(
+        first_name: "Vinsmoke",
+        last_name: "Sanji",
+        email: "mluffy@aol.com",
+        street_address: "1234",
+        city: "Baratie",
+        state: "North Blue",
+        zip: 38383
+      )
+      binding.pry
+      expect(customer2).not_to be_valid
+      expect(customer2.errors[:email]).to include("Email is in use, try another.")
+    end
+  end
 end
